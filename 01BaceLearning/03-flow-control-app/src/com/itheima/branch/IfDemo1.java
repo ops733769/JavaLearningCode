88cd68d039bb6154fcacc1b682f22c891685430c
lai_workstation <lai@tagsec.cn>
2024-02-22 11:12:25 +0800

    开始学习程序流程控制

--- 01BaceLearning/03-flow-control-app/src/com/itheima/branch/IfDemo1.java
+++ 01BaceLearning/03-flow-control-app/src/com/itheima/branch/IfDemo1.java
@@ -4,7 +4,7 @@ public class IfDemo1 {
     public static void main(String[] args) {
         // 目标：需要同学们掌握if分支三种形式的用法和执行流程。
         // 需求：测量用户体温，发现体温高于37度就报警。
-        double t = 36.9;
+        double t = 37.9;
         if(t > 37){
             System.out.println("这个人的温度异常，把他赶紧带走~~");
         }
@@ -19,7 +19,7 @@ public class IfDemo1 {
         }
 
         // 需求3：某个公司有一个绩效系统，根据员工的打分输出对应的绩效级别。[0,60) D  [60,80) C [80,90) B [90,100] A
-        int score = 298;
+        int score = 68;
         if(score >= 0 && score < 60) {
             System.out.println("您的绩效级别是： D");
         }else if(score >= 60 && score < 80){
